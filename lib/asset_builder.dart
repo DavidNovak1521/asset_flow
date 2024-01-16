ba75757a9f566ba206d13b48854bacd1c43a6685
DavidNovak1521 <davidnovak1521@gmail.com>
2024-01-16 15:35:58 +0100

    Generating from custom list of strings working.

--- lib/asset_builder.dart
+++ lib/asset_builder.dart
@@ -4,25 +4,30 @@ import 'package:build/build.dart';
 class AssetBuilder implements Builder {
   @override
   Future build(BuildStep buildStep) async {
-    final inputId = buildStep.inputId;
-    final sourceContent = await buildStep.readAsString(inputId);
+    List<String> imagePaths = [
+      'assets/animations/coming_soon.json',
+      'assets/images/logo.png',
+      'assets/images/logo.svg',
+    ];
 
     String fileContent = _header;
-    for (final path in sourceContent.split('\n')) {
+    for (final path in imagePaths) {
       fileContent += returnVariableFromPath(path);
     }
     fileContent += _footer;
 
-    final outputId = AssetId(inputId.package, 'lib/assets.dart');
+    final outputId = AssetId('asset_flow', _path);
 
     await buildStep.writeAsString(outputId, fileContent);
   }
 
   @override
   final buildExtensions = const {
-    '.txt': ['.dart']
+    r'$package$': [_path]
   };
 }
+
+const _path = 'lib/src/resources/assets/asset_flow.dart';
 
 const _header = '''  
 // GENERATED CODE - DO NOT MODIFY BY HAND
